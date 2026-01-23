13af4e718f1611e925e398fa1a6bc839099b52e6
Stefano Agbodan <mystefano95@gmail.com>
2026-01-23 14:38:10 +0100

    fix(error): undefined in not assignable to type number

--- packages/ts/src/arrays/twoSum.ts
+++ packages/ts/src/arrays/twoSum.ts
@@ -2,14 +2,14 @@ export function twoSum(nums: number[], target: number): [number, number] {
   const seen = new Map<number, number>();
 
   for (let i = 0; i < nums.length; i++) {
-    const needed = target - nums[i];
+    const needed = target - nums[i]!;
     const index = seen.get(needed);
 
     if (index !== undefined) {
       return [index, i];
     }
 
-    seen.set(nums[i], i);
+    seen.set(nums[i]!, i);
   }
 
   throw new Error("No solution");
